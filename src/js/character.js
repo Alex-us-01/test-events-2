d07c9cdf93aac9571e1dacce5058045e63ba91e0
Uskov Aleksandr <alex_us_01@mail.ru>
2025-12-09 18:52:19 +0300

    2.9

---
+++ src/js/character.js
@@ -0,0 +1,33 @@
+import { enemy, enemyDeath } from './game';
+
+const arena = document.getElementsByClassName('game-arena')[0];
+
+const clickOnGoblin = (event) => {
+  const goblinTarget = event.target.closest('.game-cell');
+  const goblinImg = goblinTarget.querySelector('.enemy');
+
+  if (goblinImg === enemy) {
+    enemy.remove();
+    goblinTarget.insertAdjacentElement('afterbegin', enemyDeath);
+    const pointsCount = document.getElementById('points-count');
+    pointsCount.textContent = 1 + Number(pointsCount.textContent);
+  }
+};
+
+arena.addEventListener('click', clickOnGoblin);
+
+export default function makeEnemyDeath(img) {
+  const image = document.createElement('img');
+  image.src = img;
+  image.className = 'enemy-death';
+  image.id = 'enemy-death-face';
+  return image;
+}
+
+export default function makeEnemy(img) {
+  const image = document.createElement('img');
+  image.src = img;
+  image.className = 'enemy';
+  image.id = 'enemy-face';
+  return image;
+}
